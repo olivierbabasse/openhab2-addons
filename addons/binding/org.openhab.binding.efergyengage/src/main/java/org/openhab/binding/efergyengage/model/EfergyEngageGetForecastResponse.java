49265f21acbea3b26f883f10a052a07eed2ab74b
Ondrej Pecta <opecta@gmail.com>
2017-08-05 00:43:57 +0200

    Fixed copyright headers

--- addons/binding/org.openhab.binding.efergyengage/src/main/java/org/openhab/binding/efergyengage/model/EfergyEngageGetForecastResponse.java
+++ addons/binding/org.openhab.binding.efergyengage/src/main/java/org/openhab/binding/efergyengage/model/EfergyEngageGetForecastResponse.java
@@ -1,8 +1,16 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.efergyengage.model;
 
 /**
  * The {@link EfergyEngageGetInstantResponse} represents the model of
- * the montly money spending estimation response.
+ * the monthly money spending estimation response.
  *
  * @author Ondrej Pecta - Initial contribution
  */
